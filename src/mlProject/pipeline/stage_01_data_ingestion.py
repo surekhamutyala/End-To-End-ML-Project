bbc8d5f42f0f00ecd8cb5b2593b8d509f8c05f07
SurekhaMutyala <saisurekha12@gmail.com>
2024-06-30 02:45:49 +0530

    data ingestion added

--- src/mlProject/pipeline/stage_01_data_ingestion.py
+++ src/mlProject/pipeline/stage_01_data_ingestion.py
@@ -1,7 +1,6 @@
-from src.mlProject import logger
-from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.config.configuration import ConfigurationManager
 from src.mlProject.components.data_ingestion import DataIngestion
+from mlProject import logger
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -22,8 +21,8 @@ if __name__ == '__main__':
         logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
         obj = DataIngestionTrainingPipeline()
         obj.main()
-        logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x==========x')
+        logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<< \n\n')
     except Exception as e:
         logger.exception(e)
-        raise e
+    raise e
 
